7c46ecf56fb96f44bc0ee09394db1e7d984c77be
James Coliz <jcoliz@outlook.com>
2022-05-04 12:23:12 -0700

    build: Rename Client project to FrontEnd

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using PlaidQuickstartBlazor.Client;
+using PlaidQuickstartBlazor.FrontEnd;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
