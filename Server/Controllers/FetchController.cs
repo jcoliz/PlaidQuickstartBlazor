1d4ff329d1f379a0365201aba23c2ec7e144258e
James Coliz <jcoliz@outlook.com>
2022-05-04 16:57:10 -0700

    Stub backend apis for all other items

--- Server/Controllers/FetchController.cs
+++ Server/Controllers/FetchController.cs
@@ -4,7 +4,7 @@ using PlaidQuickstartBlazor.Shared;
 namespace PlaidQuickstartBlazor.Server.Controllers;
 
 [ApiController]
-[Route("api/[controller]/[action]")]
+[Route("[controller]/[action]")]
 [Produces("application/json")]
 public class FetchController : ControllerBase
 {
@@ -18,7 +18,7 @@ public class FetchController : ControllerBase
     [HttpGet]
     public IList<string[]> Auth()
     {
-        _logger.LogInformation("GetAuth");
+        _logger.LogInformation("Auth");
 
         var result = new List<string[]>() 
         { 
@@ -30,4 +30,95 @@ public class FetchController : ControllerBase
 
         return result;
     }
+    [HttpGet]
+    public IList<string[]> Transactions()
+    {
+        _logger.LogInformation("Transactions");
+
+        var result = new List<string[]>()
+        {
+            new[] { "A", "B", "C", "D", "E" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+        };
+
+        return result;
+    }
+    [HttpGet]
+    public IList<string[]> Identity()
+    {
+        _logger.LogInformation("Identity");
+
+        var result = new List<string[]>()
+        {
+            new[] { "A", "B", "C", "D", "E" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+        };
+
+        return result;
+    }
+    [HttpGet]
+    public IList<string[]> Holdings()
+    {
+        _logger.LogInformation("Holdings");
+
+        var result = new List<string[]>()
+        {
+            new[] { "A", "B", "C", "D", "E" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+        };
+
+        return result;
+    }
+    [HttpGet]
+    public IList<string[]> Investments_Transactions()
+    {
+        _logger.LogInformation("Investments_Transactions");
+
+        var result = new List<string[]>()
+        {
+            new[] { "A", "B", "C", "D", "E" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+        };
+
+        return result;
+    }
+    [HttpGet]
+    public IList<string[]> Balance()
+    {
+        _logger.LogInformation("Balance");
+
+        var result = new List<string[]>()
+        {
+            new[] { "A", "B", "C", "D", "E" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+        };
+
+        return result;
+    }
+    [HttpGet]
+    public IList<string[]> Liabilities()
+    {
+        _logger.LogInformation("Liabilities");
+
+        var result = new List<string[]>()
+        {
+            new[] { "A", "B", "C", "D", "E" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+            new[] { "1", "2", "3", "4", "5" },
+        };
+
+        return result;
+    }
+
 }
