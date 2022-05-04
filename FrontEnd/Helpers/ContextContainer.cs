7582e2cb78d7f010c81ab55f584c4e2f1cf91e71
James Coliz <jcoliz@outlook.com>
2022-05-04 13:09:18 -0700

    feat: Initial click flow for header

---
+++ FrontEnd/Helpers/ContextContainer.cs
@@ -0,0 +1,53 @@
+namespace PlaidQuickstartBlazor.FrontEnd.Helpers;
+
+public class ContextContainer
+{
+    public bool isItemAccess
+    {
+        get => _isItemAccess;
+        set
+        {
+            _isItemAccess = value;
+            NotifyStateChanged();
+        }
+    }
+    private bool _isItemAccess;
+
+    public bool linkSuccess
+    {
+        get => _linkSuccess;
+        set
+        {
+            _linkSuccess = value;
+            NotifyStateChanged();
+        }
+    }
+    private bool _linkSuccess;
+
+    public string? accessToken
+    {
+        get => _accessToken;
+        set
+        {
+            _accessToken = value;
+            NotifyStateChanged();
+        }
+    }
+    private string? _accessToken;
+
+    public string? itemId
+    {
+        get => _itemId;
+        set
+        {
+            _itemId = value;
+            NotifyStateChanged();
+        }
+    }
+    private string? _itemId;
+
+
+    public event Action? OnChange;
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PlaidQuickstartBlazor.FrontEnd;
+using PlaidQuickstartBlazor.FrontEnd.Helpers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<ContextContainer>();
 
 await builder.Build().RunAsync();
