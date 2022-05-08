71522715ae2c6d49e7887a6fb4cc1a825a8e502f
James Coliz <jcoliz@outlook.com>
2022-05-08 13:02:55 -0700

    test(func): Add data test rows for all working products

--- Tests.Functional/ProductsTest.cs
+++ Tests.Functional/ProductsTest.cs
@@ -18,33 +18,42 @@ public class ProductsTest: FunctionalTest
     /// [User Can] Access the product {name}
     /// </summary>
     [DataRow("Auth", 4, 2)]
+    [DataRow("Transactions", 5, null)]
+    [DataRow("Identity", 4, 9)]
+    [DataRow("Assets", 4, 9)]
+    [DataRow("Balance", 3, 9)]
+    [DataRow("Investments", 5, 12)]
+    [DataRow("InvestmentsTransactions", 4, null)]
+    [DataRow("Liabilities", 3, 3)]
     [DataTestMethod]
-    public async Task Product(string name, int expected_cols_count, int expected_rows_count)
+    public async Task Product(string name, int expected_cols_count, int? expected_rows_count)
     {
         // Given: On the root of the site
         await Page!.GotoAsync(TestContext?.Properties?["webAppUrl"] as string ?? throw new ApplicationException());
 
         // And: Link flow complete 
-        await Page!.WaitForSelectorAsync("data-test-id=launch-link");
         await Page!.ClickAsync("data-test-id=launch-link");
         await Page!.WaitForLoadStateAsync( LoadState.DOMContentLoaded );
-        await SaveScreenshotAsync("0-Ready");
+        await SaveScreenshotAsync($"{name}-0-Ready");
 
         // When: Clicking on "Send Request" within the "{name}}" product
         await Page!.ClickAsync($"data-test-id=request-{name}");
 
         // Then: A table of results is populated
         await Page!.WaitForSelectorAsync("data-test-id=Table");
-        await SaveScreenshotAsync("1-Loaded");
+        await SaveScreenshotAsync($"{name}-1-Loaded");
 
         // And: The shape of the table matches our expectations
         var actual_cols = Page!.Locator("data-test-id=Table >> thead >> th");
         var actual_cols_count = await actual_cols.CountAsync();
         Assert.AreEqual(expected_cols_count,actual_cols_count);
 
-        var actual_rows = Page!.Locator("data-test-id=Table >> tbody >> tr");
-        var actual_rows_count = await actual_rows.CountAsync();
-        Assert.AreEqual(expected_rows_count,actual_rows_count);
+        if (expected_rows_count.HasValue)
+        {
+            var actual_rows = Page!.Locator("data-test-id=Table >> tbody >> tr");
+            var actual_rows_count = await actual_rows.CountAsync();
+            Assert.AreEqual(expected_rows_count.Value,actual_rows_count);
+        }
     }
 
     #endregion
