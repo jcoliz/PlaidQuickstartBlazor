e0079b10d5c997ea817d22d7e24ffa48b2620392
James Coliz <jcoliz@outlook.com>
2022-05-07 17:14:25 -0700

    style: Comments and cleanups

--- FrontEnd/Program.cs
+++ FrontEnd/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PlaidQuickstartBlazor.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-//builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<ContextContainer>();
